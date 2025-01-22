6c7a6171a8868adca365743c792a85c82207e0d9
SafaCubuk <116181836+SafaCubuk@users.noreply.github.com>
2025-01-22 19:35:31 +0300

    deneme1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hellfgxcxvfvfghgfgfgfgfgfghfgfghfgfgdfgdfgdsfgfdfsdfdsfdffgddfsdfdsfdsfsdfdsfsdfgfdgfdgfdo World!'),
+          child: Text('Hellfgxcxvfvfghgfgfgfgfgfghfgfgdffgddfsdfdsfdsfsdfdsfsdfgfdgfdgfdo World!'),
         ),
       ),
     );
