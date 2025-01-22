a7706642a9428852b832a54a9b6341f0697684ae
SafaCubuk <116181836+SafaCubuk@users.noreply.github.com>
2025-01-22 19:18:21 +0300

    evcilhayvan

---
+++ README.md
@@ -0,0 +1,3 @@
+# evcilhayvantakip
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/evcilhayvantakip/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.evcilhayvantakip
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
